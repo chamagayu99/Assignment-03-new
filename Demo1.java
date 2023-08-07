4ed785d11261bfe67cea02bddcb6ac9f9e75ef33
gayathri <chamalkagayathri@gmail>
2023-08-08 00:18:39 +0530

    task completed

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+public class Demo1 {
+
+    public static void main(String[] args) {
+        
+        String red= "SE-1456";
+        String r=red.substring(2, 4);
+        System.out.println(r);
+    }
+    
+}
